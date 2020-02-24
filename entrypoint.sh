17fafac1f6ee46dda3d069d5bbe3a4ecedb7860f
Gergely Szabo <gergely.szabo@origoss.com>
2020-02-24 16:11:03 +0100

    openvpn debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,7 @@ echo $INPUT_OVPNUSERNAME > $HOME/$INPUT_OVPNPASSFILENAME
 echo $INPUT_OVPNPASSWORD >> $HOME/$INPUT_OVPNPASSFILENAME
 cat $HOME/$INPUT_OVPNPASSFILENAME
 cd $HOME
-openvpn --config client.ovpn --daemon
+echo "Starting openvpn client"
+openvpn --config client.ovpn --log /dev/stdout --daemon
+ps ax | grep openvpn
 curl https://web.01.oscluster.net:8443/
