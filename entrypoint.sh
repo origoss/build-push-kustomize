9fed711d4c70d63d72524821cf673c8686691673
Gergely Szabo <gergely.szabo@origoss.com>
2020-02-24 15:19:25 +0100

    not using home/secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "----- env ------"
 printenv
 echo "decrypting client.ovpn"
 gpg --quiet --batch --yes --decrypt --passphrase="$INPUT_GPGPASSPHRASE" \
-    --output $HOME/secrets/client.ovpn $INPUT_OVPNPATH
-cat $HOME/secrets/client.ovpn
+    --output $HOME/client.ovpn $INPUT_OVPNPATH
+cat $HOME/client.ovpn
