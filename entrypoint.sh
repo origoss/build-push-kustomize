7fb824d393964eaf38ce3101bf6ba8c5ec43a86e
Gergely Szabo <gergely.szabo@origoss.com>
2020-02-24 15:42:08 +0100

    ovpn usename and passwords added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ echo "decrypting client.ovpn"
 gpg --quiet --batch --yes --decrypt --passphrase="$INPUT_GPGPASSPHRASE" \
     --output $HOME/client.ovpn $INPUT_OVPNPATH
 cat $HOME/client.ovpn
+echo $INPUT_OVPNUSERNAME > $HOME/pass.txt
+echo $INPUT_OVPNPASSWORD >> $HOME/pass.txt
+cat $HOME/pass.txt
