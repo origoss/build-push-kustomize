77ed88bc309fdf90bbe63fbe34f41de1e5e4cbb6
Gergely Szabo <gergely.szabo@origoss.com>
2020-03-10 16:26:55 +0100

    rewrite

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM alpine:3.11.3
 
-COPY LICENSE README.md /
+COPY LICENSE README.md registry-config.yml.tmpl /
+
+RUN apk --no-cache add bash gettext git
+
+ADD https://github.com/uber/makisu/releases/download/v0.1.14/makisu-v0.1.14 /usr/local/bin/makisu
+ADD https://raw.githubusercontent.com/uber/makisu/master/assets/cacerts.pem /makisu-internal/certs/cacerts.pem
+ADD https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.5.4/kustomize_v3.5.4_linux_amd64.tar.gz /tmp/kustomize.tar.gz
+
+RUN chmod u+x /usr/local/bin/makisu && \
+  cd /tmp/                          && \
+  tar xf /tmp/kustomize.tar.gz      && \
+  mv /tmp/kustomize /usr/local/bin
 
 COPY entrypoint.sh /entrypoint.sh
-
-RUN apk --no-cache add openvpn gnupg curl
-
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,40 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "----- env ------"
-printenv
-echo "decrypting client.ovpn"
-gpg --quiet --batch --yes --decrypt --passphrase="$INPUT_GPGPASSPHRASE" \
-    --output $HOME/client.ovpn $INPUT_OVPNPATH
-cat $HOME/client.ovpn
-echo $INPUT_OVPNUSERNAME > $HOME/$INPUT_OVPNPASSFILENAME
-echo $INPUT_OVPNPASSWORD >> $HOME/$INPUT_OVPNPASSFILENAME
-cat $HOME/$INPUT_OVPNPASSFILENAME
-cd $HOME
-echo "Starting openvpn client"
-openvpn --config client.ovpn --log /dev/stdout --daemon
-echo "Waiting for openvpn setup..."
-sleep 10
-ps ax | grep openvpn
-curl https://web.01.oscluster.net:8443/
+set -x
+
+# INPUT_IMAGE_NAMES="first second"
+# INPUT_IMAGE_TAG_PREFIX="origoss/github-actions-demo:test-"
+# INPUT_IMAGE_TAG_SUFFIX="-$(date +%s)"
+# INPUT_DOCKERFILE_SUFFIX=".kube"
+# export INPUT_DOCKER_USERNAME=gregorigoss
+# export INPUT_DOCKER_PASSWORD="dbfec147-2062-48f6-bb4c-281fe3924d1a"
+# INPUT_KUSTOMIZE_REPO_PATH=kustomize-repo/manifests/overlays/staging-devops
+# INPUT_GIT_USER_NAME="Github Actions"
+# INPUT_GIT_USER_EMAIL="noreply@test.io"
+
+cat /registry-config.yml.tmpl | envsubst | tee /registry-config.yml
+
+cd /github/home
+
+for f in $INPUT_IMAGE_NAMES
+do
+    makisu build \
+           -f Dockerfile.${f}${INPUT_DOCKERFILE_SUFFIX}     \
+           -t ${INPUT_IMAGE_TAG_PREFIX}${f}${INPUT_IMAGE_TAG_SUFFIX}    \
+           --registry-config=/registry-config.yml \
+           --push index.docker.io                      \
+           .
+done
+
+if [[ -d $INPUT_KUSTOMIZE_REPO_PATH ]]
+then
+    cd $INPUT_KUSTOMIZE_REPO_PATH
+    for f in $INPUT_IMAGE_NAMES
+    do
+        kustomize edit set image $f=${INPUT_IMAGE_TAG_PREFIX}${f}${INPUT_IMAGE_TAG_SUFFIX}
+    done
+fi
+
+git config user.name $INPUT_GIT_USER_NAME
+git config user.email $INPUT_GIT_USER_EMAIL
+git commit -am "Updating image tags for '${INPUT_IMAGE_NAMES}' using tag suffix ${INPUT_IMAGE_TAG_SUFFIX}"
